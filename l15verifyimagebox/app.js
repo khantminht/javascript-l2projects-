d8d4e801d78a9f65c17a0f875f2fdd63197ae47b
khantminht <khantminht.bizs@gmail.com>
2022-09-20 08:47:30 +0630

    20DT

--- l15verifyimagebox/app.js
+++ l15verifyimagebox/app.js
@@ -1,6 +1,6 @@
 const database = [
     {
-    question:"Choose Traffic Light",
+    question:"Choose Traffic Light!",
     a:"./img/traffic.jpg",
     b:"./img/mountain.jpg",
     c:"./img/ambulance.jpg",
@@ -10,7 +10,7 @@ const database = [
     },
 
     {
-        question:"Choose Mountain",
+        question:"Choose Mountain!",
         a:"./img/ambulance.jpg",
         b:"./img/mountain.jpg",
         c:"./img/traffic.jpg",
@@ -20,7 +20,7 @@ const database = [
     },
 
     {
-        question:"Choose Ambulance",
+        question:"Choose Ambulance!",
         a:"./img/ambulance.jpg",
         b:"./img/airport.jpg",
         c:"./img/traffic.jpg",
@@ -30,7 +30,7 @@ const database = [
     },
 
     {
-        question:"Choose Airport",
+        question:"Choose Airport!",
         a:"./img/traffic.jpg",
         b:"./img/mountain.jpg",
         c:"./img/ambulance.jpg",
@@ -41,27 +41,117 @@ const database = [
     
 ];
 
-console.log(database);
+// console.log(database);
 
 const getcontainer = document.querySelector('.container');
 const getquestion = document.querySelector('.question');
 const getanswers = document.querySelectorAll('.answer');
-
-console.log(getanswers);
+// console.log(getanswers);
 
 const geta_img = document.getElementById('a_img');
 const getb_img = document.getElementById('b_img');
 const getc_img = document.getElementById('c_img');
 const getd_img = document.getElementById('d_img');
 
-console.log(geta_img);
+const getbtn = document.querySelector('.btn');
+// console.log(geta_img);
+
+let currentidx = 0;
+let score = 0;
 
 startquestion();
 
 function startquestion(){
-    getquestion.textContent = database[0].question;
-    geta_img.src = database[0].a;
-    getb_img.src = database[0].b;
-    getc_img.src = database[0].c;
-    getd_img.src = database[0].d;
+
+    removeselects();
+
+    const currentques = database[currentidx];
+    // console.log(currentques);
+
+    getquestion.textContent = currentques.question;
+    geta_img.src = currentques.a;
+    getb_img.src = currentques.b;
+    getc_img.src = currentques.c;
+    getd_img.src = currentques.d;
+}
+
+function getsingleanswer(){
+
+    let answer;
+
+    getanswers.forEach(function(getanswer){
+        // console.log(getanswer);
+        // console.log(getanswer.id);
+
+        if(getanswer.checked){
+            // console.log(getanswer.id);
+            answer = getanswer.id;
+        }
+    });
+
+    return answer;
+}
+// getsingleanswer();
+
+getbtn.addEventListener('click',function(){
+    // console.log('iam working');
+
+    const answer = getsingleanswer();
+    // console.log(answer);
+
+    if(answer){
+        // console.log(answer);
+
+        if(answer === database[currentidx].correctanswer){
+            score++;
+        }
+
+        currentidx++;
+
+        if(currentidx < database.length){
+            startquestion();
+        }else{
+            // console.log(score);
+
+            getcontainer.innerHTML = `
+                <h3>Total Score : ${score*25} </h3>
+                <h4>You answered correctly at ${score} / ${database.length} questions. </h4>
+                <!-- <button type="button" class="btn" ondblclick="location.reload()">Double Click to Reload</button> -->
+
+                <button type="button" class="btn" onclick="doubleclick();">Double Click to Reload</button>
+            
+            `;
+        }
+    }else{
+        window.alert('Choose One Answer');
+    }
+});
+
+
+function removeselects(){
+    getanswers.forEach(function(getanswer){
+        return getanswer.checked = false;
+    });
+
+}
+
+let clicktimes = 0;
+
+function doubleclick(){
+    // console.log('iam working');
+
+    if(clicktimes === 0){
+        clicktimes = new Date().getTime();
+
+    }else{
+
+        if((new Date().getTime() - clicktimes) < 800){
+            // console.log('hello');
+
+            location.reload();
+            clicktimes =0;
+        }else{
+            clicktimes = new Date().getTime();
+        }
+    }
 }
